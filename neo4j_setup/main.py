c56f2223a034ade619c7c95fa0ad233cc8b858e8
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2026-08-23 22:27:49 +0300

    feat: import all nodes and edges from the Microsoft SQL Server database

--- neo4j_setup/main.py
+++ neo4j_setup/main.py
@@ -1,7 +1,7 @@
 import os
 
 import pandas as pd
-from neo4j import GraphDatabase
+from neo4j import Driver, GraphDatabase, Query
 from sqlalchemy import Engine, create_engine, text
 
 
@@ -24,6 +24,51 @@ def get_graph_connection_info() -> tuple[str, tuple[str, str]]:
     return uri, (username, password)
 
 
+def merge_schema_information(driver: Driver, schema_df: pd.DataFrame):
+    driver.execute_query(
+        "CREATE CONSTRAINT table_name_unique IF NOT EXISTS FOR (t:Table) REQUIRE t.name IS UNIQUE;"
+    )
+
+    unique_tables = [{"tableName": t} for t in schema_df["TableName"].unique()]
+    driver.execute_query(
+        """
+        UNWIND $tables AS row
+        MERGE (:Table {name: row.tableName})
+    """,
+        tables=unique_tables,
+    )
+
+    col_df = schema_df[["TableName", "ColumnName", "DataType", "IsPrimaryKey"]].copy()
+    col_df["IsPrimaryKey"] = col_df["IsPrimaryKey"].fillna(False).astype(bool)
+    columns_batch = col_df.to_dict("records")
+    driver.execute_query(
+        """
+        UNWIND $columnsBatch AS row
+        MATCH (t:Table {name: row.TableName})
+        MERGE (t)-[:HAS_COLUMN]->(c:Column {name: row.ColumnName})
+        SET 
+            c.data_type = row.DataType,
+            c.is_primary_key = row.IsPrimaryKey
+    """,
+        columnsBatch=columns_batch,
+    )
+
+    fk_df = schema_df[
+        ["TableName", "ColumnName", "ReferencedTableName", "ReferencedColumnName"]
+    ].dropna()
+    fks_batch = fk_df.to_dict("records")
+    if fks_batch:
+        driver.execute_query(
+            """
+            UNWIND $fksBatch AS row
+            MATCH (source_table:Table {name: row.TableName})-[:HAS_COLUMN]->(source_col:Column {name: row.ColumnName})
+            MATCH (target_table:Table {name: row.ReferencedTableName})-[:HAS_COLUMN]->(target_col:Column {name: row.ReferencedColumnName})
+            MERGE (source_col)-[:REFERENCES]->(target_col)
+        """,
+            fksBatch=fks_batch,
+        )
+
+
 def populate_knowledge_graph() -> None:
     engine = get_database_engine()
     uri, auth = get_graph_connection_info()
@@ -35,6 +80,7 @@ def populate_knowledge_graph() -> None:
         driver.verify_connectivity()
         get_db_schema_query = text(get_db_schema_file.read())
         schema_df = pd.read_sql_query(get_db_schema_query, conn)
+        merge_schema_information(driver, schema_df)
         assert len(schema_df) != 0
 
 
