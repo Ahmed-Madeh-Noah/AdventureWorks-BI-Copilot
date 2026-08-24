592a2eea837d5697dcc069bdef8451a26eed18cb
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2026-08-24 10:23:57 +0300

    refactor: remove context rertiever

--- mssql/check_setup.sql
+++ mssql/check_setup.sql
@@ -21,12 +21,3 @@ IF NOT EXISTS (
     WHERE
         name = '$(MSSQL_DBGATE_READER_USERNAME)'
 ) RAISERROR ('DBGATE user is not ready.', 16, 1);
-
-IF NOT EXISTS (
-    SELECT
-        *
-    FROM
-        sys.server_principals
-    WHERE
-        name = '$(MSSQL_CONTEXT_RETRIEVER_READER_USERNAME)'
-) RAISERROR ('CONTEXT_RETRIEVER user is not ready.', 16, 1);
